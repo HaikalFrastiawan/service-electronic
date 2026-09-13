b9b9aabaea712fa01884f79e0d5afc6d21a33b45
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-09-13 13:02:10 +0700

    setup

---
+++ src/test/java/service/electronic/ElectronicApplicationTests.java
@@ -0,0 +1,13 @@
+package service.electronic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
